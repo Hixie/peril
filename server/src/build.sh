32fe53f8c2a381c2d93cde4cb759bd80ae097373
Ian Hickson <ian@hixie.ch>
2015-07-07 05:37:22 +0000

    process-turn test input

--- server/src/build.sh
+++ server/src/build.sh
@@ -1,5 +1,12 @@
 #MAIN="test" MODE="DEBUG" lib/compile.sh; exit
-#MODE="RELEASE"
 MODE="DEBUG"
 MAIN="start-game" MODE=$MODE TESTCMD="src/tests/test-start-game.sh" lib/compile.sh || exit 1
 MAIN="process-turn" MODE=$MODE TESTCMD="src/tests/test-process-turn.sh" lib/compile.sh || exit 1
+
+# 32-bin binaries:
+#MODE="RELEASE
+#export PATH=/usr/local/bin:/usr/bin:/bin:~/bin:~/bin/fpc-32/trunk/bin/:~/bin/gdb/bin/:~/bin/valgrind/bin/bin
+#cp -f ~/bin/fpc-32/fpc.cfg ~/.fpc.cfg
+#MAIN="start-game" MODE=$MODE TESTCMD="echo" lib/compile.sh || exit 1
+#MAIN="process-turn" MODE=$MODE TESTCMD="echo" lib/compile.sh || exit 1
+#cp -f ~/bin/fpc/fpc.cfg ~/.fpc.cfg
